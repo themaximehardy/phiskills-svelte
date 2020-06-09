2ab16e04cb7d538a6caeaa4b550525d807e137f0
Max Hardy <max@hardy.work>
2020-06-09 16:23:47 +0100

    values added

--- src/components/Values.svelte
+++ src/components/Values.svelte
@@ -4,38 +4,20 @@
     import Title from "./shared/Title.svelte"
 
     const data = getContext("data")
+
+    const {values} = data
 </script>
 
 <section class="py-16 px-4">
     <Title title="our values"/>
     <div class="flex flex-wrap max-w-5xl -mx-4 -mb-8 md:mx-auto">
-        <div class="w-full px-4 mb-8 md:w-1/2">
-            <div class="h-full p-6 bg-white rounded border-t-4 border-indigo-500 shadow-md">
-                <h3 class="text-2xl mb-3">Transparency </h3>
-                <p>Forget about paper jams with our increased stability product.
-                    Ideal for high-quality printing and photocopying.</p>
+        {#each values.content as item}
+            <div class="w-full px-4 mb-8 md:w-1/2">
+                <div class="h-full p-6 bg-white rounded border-t-4 border-indigo-500 shadow-md">
+                    <h3 class="text-2xl mb-3">{item.title}</h3>
+                    <p>{item.text}</p>
+                </div>
             </div>
-        </div>
-        <div class="w-full px-4 mb-8 md:w-1/2">
-            <div class="h-full p-6 bg-white rounded border-t-4 border-indigo-500 shadow-md">
-                <h3 class="text-2xl mb-3">Empowerment </h3>
-                <p>Multi-functional paper for various office needs. Snow-white
-                    sheets will shine bright like diamond for many years.</p>
-            </div>
-        </div>
-        <div class="w-full px-4 mb-8 md:w-1/2">
-            <div class="h-full p-6 bg-white rounded border-t-4 border-indigo-500 shadow-md">
-                <h3 class="text-2xl mb-3">Agility</h3>
-                <p>The best combination of quality and economy comes with our
-                    paper. The offer includes wides ranges of density.</p>
-            </div>
-        </div>
-        <div class="w-full px-4 mb-8 md:w-1/2">
-            <div class="h-full p-6 bg-white rounded border-t-4 border-indigo-500 shadow-md">
-                <h3 class="text-2xl mb-3">XXX</h3>
-                <p>The offer includes paper in wide range of density and sizes.
-                    Ideal for high-quality printing and photocopying.</p>
-            </div>
-        </div>
+        {/each}
     </div>
 </section>
